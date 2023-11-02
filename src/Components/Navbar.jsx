8c9a8bd4ff11938f719a31714d64c5f98e6f967f
S NAVEEN KUMAR <navee1110@gmail.com>
2023-11-02 19:34:01 +0530

    hero_section_created

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -28,9 +28,9 @@ export const Navbar = () => {
         fontWeight: "500",
         border: "1px solid black",
       }}
-      className={`flex items-center justify-between px-10 py-4 border-b-2  sticky top-0 left-0 w-full	   z-[999] `}
+      className={`flex items-center justify-between px-10 py-4 border-b-2 sticky top-0 left-0 w-full z-[999] `}
     >
-      <div className="flex items-center justify-between max-[900px]:w-full  mr-5 ">
+      <div className="flex items-center justify-between max-[900px]:w-full mr-5 ">
         <Link
           className="nav-link home"
           to="home"
@@ -46,6 +46,7 @@ export const Navbar = () => {
           <HiMenuAlt1 className="text-2xl" />
         </span>
       </div>
+      
       <div className="flex items-center gap-12 max-[900px]:hidden">
         <Link
           to="about"
@@ -87,18 +88,17 @@ export const Navbar = () => {
         >
           <div className={styles.nav}>contact</div>
         </Link>
-        <button className="nav-link resume" id="resume-button-1"  onClick={openNewTab}>
-          <div   className={styles.nav}>Resume</div>
+        <button className="nav-link resume" id="resume-button-1" onClick={openNewTab}>
+          <div className={styles.nav}>Resume</div>
         </button>
       </div>
+      
       <div className="relative min-[900px]:hidden">
         <div
-          className={`flex gap-4 w-[170px] flex-col fixed top-[4.3rem] -right-3 p-1 px-3 shadow-[0px_13px_29px_0px_rgba(100,100,111,0.4)] bg-white h-[100vh] ${
-            isMobileMenuVisible ? "" : "-right-[300px]"
-          } transition-all duration-500`}
+          className={`flex gap-4 w-[170px] flex-col fixed top-[4.3rem] -right-3 p-1 px-3 shadow-[0px_13px_29px_0px_rgba(100,100,111,0.4)] bg-white h-[100vh] ${isMobileMenuVisible ? "" : "-right-[300px]"} transition-all duration-500`}
         >
           <Link
-            className="nav-link about"
+            className="nav-link about" 
             to="about"
             smooth={true}
             duration={1000}
