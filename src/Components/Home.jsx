25be484a9fd7fe3d37646795c77332b463464666
S NAVEEN KUMAR <navee1110@gmail.com>
2023-11-02 18:12:25 +0530

    hero_section created

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,110 +1,138 @@
 import React from "react";
-import {
-  Box,
-  Button,
-  Flex,
-  Image,
-  Link,
-  Text,
-  VStack,
-  HStack,
-} from "@chakra-ui/react";
-import { AiOutlineGithub, AiFillLinkedin } from "react-icons/ai";
+import styled from "styled-components";
 import { RiDownloadLine } from "react-icons/ri";
-import styles from "../Styles/Home.module.css";
+import { AiOutlineGithub } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
 
 export const Home = () => {
+  const openNewTab = () => {
+    window.open(
+      "https://drive.google.com/file/d/1M7i_EjfoYeB5odZ6TBx8yi_zD034LIBp/view?usp=sharing",
+      "_blank"
+    );
+  };
+
   return (
-    <Box
-      id="home"
-      className={`${styles.rootCont}`}
-      display="grid"
-      gridTemplateAreas={{
-        base: `"nameCont" "photoCont" "socialCont"`,
-        md: `"nameCont photoCont" "socialCont socialCont"`,
-      }}
-      gridTemplateColumns={{
-        base: "1fr",
-        md: "1fr 1fr",
-      }}
-      width={{
-        base: "90%",
-        md: "80%",
-      }}
-      margin="auto"
-      alignItems="center"
-      justifyContent="center"
-      height={{
-        base: "100vh",
-        md: "80vh",
-      }}
-    >
-      <HStack
-        className="nameCont"
-        width="100%"
-        fontSize={{
-          base: "30px",
-          md: "50px",
-        }}
-        fontWeight="800"
-        color="rgb(14, 36, 49)"
-        textAlign="center"
-        maxH={{
-          base: "50vh",
-          md: "60vh",
-        }}
-        spacing={2}
-      >
-        <div>Hi,</div>
-        <div id="user-detail-name">
-          I am{" "}
-          <Text as="span" color="rgb(8, 111, 143)">
-            S Naveen Kumar
-          </Text>
-        </div>
-        <Text>Web Developer</Text>
-        <Link href="" isExternal>
-          <Button
-            variant="solid"
-            colorScheme="teal"
-            size="sm"
-            leftIcon={<RiDownloadLine />}
+    <DIV id="home">
+      <Content id="intro">
+        <h1>Hi,</h1>
+        <h1>I am S Naveen Kumar</h1>
+        <h5>Full Stack Web Developer</h5>
+        <button id="resume-link-2" onClick={openNewTab}>
+          <div>
+            <RiDownloadLine
+              style={{
+                fontSize: "18px",
+                marginLeft: "10px",
+                marginRight: "5px",
+              }}
+            />
+          </div>
+          <div>Resume</div>
+        </button>
+        <div id="socialIcons">
+          <a
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/s-naveen-kumar-17774312a/"
+            target="_blank"
+            id="linkedInIcon"
           >
-            Resume
-          </Button>
-        </Link>
-      </HStack>
-
-      <Flex className="photoCont" gridArea="photoCont">
-        <Image
-          src="https://camo.githubusercontent.com/e4a569755580f96dce0e6d65bc761e0d9aef0fecae524ec73a1b0be60fc934fa/68747470733a2f2f7777772e6d79676f2e67652f75706c6f6164732f626c6f672f313538343032333739352e6a7067"
-          alt=""
-          className="heroImg"
-          width="100%"
-          borderRadius="5px"
-          maxH="60vh"
+            <AiFillLinkedin />
+          </a>
+          <a
+            rel="noreferrer"
+            href="https://github.com/S-Naveen-Kumar-1"
+            target="_blank"
+            id="gitIcon"
+          >
+            <AiOutlineGithub />
+          </a>
+        </div>
+      </Content>
+      <Content id="codeImg">
+        <img
+            src="https://camo.githubusercontent.com/e4a569755580f96dce0e6d65bc761e0d9aef0fecae524ec73a1b0be60fc934fa/68747470733a2f2f7777772e6d79676f2e67652f75706c6f6164732f626c6f672f313538343032333739352e6a7067"
+            alt=""
         />
-      </Flex>
-
-      <Flex
-        className="socialCont"
-        gridArea="socialCont"
-        width="100%"
-        justifyContent="center"
-        alignItems="center"
-        spacing={4}
-        marginTop={{
-          base: "20px",
-          md: "0",
-        }}
-      >
-        <Link href="" isExternal>
-          <AiFillLinkedin className="linkedInIcon" />
-        </Link>
-        <Link href="" isExternal>
-          <AiOutlineGithub className="gitIcon" />
-        </Link>
-      </Flex>
-    </Box>
+      </Content>
+    </DIV>
   );
 };
+
+const DIV = styled.div`
+  display: flex;
+  border: 1px solid red;
+  justify-content: center;
+  gap: 5%;
+  padding-top: 80px;
+  margin: 5%;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const Content = styled.div`
+  width: 50%;
+  border: 1px solid red;
+
+  h1 {
+    font-size: 50px;
+    font-weight: 800;
+  }
+  h5 {
+    font-size: 40px;
+    font-weight: 600;
+    color: #495a92;
+  }
+
+  button {
+    border: 3px solid #495a92;
+    background-color: #495a92;
+    width: auto; /* Adjust the width to "auto" */
+    color: white;
+    height: 11%;
+    border-radius: 5%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  #socialIcons {
+    display: flex;
+  }
+  #linkedInIcon {
+    font-size: 40px;
+    margin: 0 10px;
+    cursor: pointer;
+    color: rgb(8, 111, 143);
+    margin: 2%;
+    transition: color 0.3s;
+  }
+  #linkedInIcon:hover {
+    color: rgb(17, 121, 224) !important;
+  }
+  #gitIcon {
+    font-size: 40px;
+    margin: 2%;
+    cursor: pointer;
+    color: rgb(80, 80, 80);
+    transition: color 0.3s;
+  }
+  #gitIcon:hover {
+    color: rgb(17, 121, 224) !important;
+  }
+  @media (max-width: 768px) {
+    width: 100%;
+    h1 {
+      font-size: 30px;
+    }
+    h5 {
+      font-size: 30px;
+      font-weight: 400;
+    }
+    button {
+      width: auto; /* Adjust the width to "auto" for mobile screens */
+      height: 40px;
+      font-size: 14px;
+    }
+  }
+  `
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,20 +1,52 @@
+import { useState } from "react";
 import styles from "../Styles/Navbar.module.css";
 import { Link } from "react-scroll";
+import { HiMenuAlt1 } from "react-icons/hi";
+
 export const Navbar = () => {
+  const [isMobileMenuVisible, setIsMobileMenuVisible] = useState(false);
+
+  const handleMobileMenu = () => {
+    setIsMobileMenuVisible(!isMobileMenuVisible);
+  };
+
+  const openNewTab = () => {
+    window.open(
+      "https://drive.google.com/file/d/1M7i_EjfoYeB5odZ6TBx8yi_zD034LIBp/view?usp=sharing",
+      "_blank"
+    );
+  };
+
   return (
-    <div id="nav-menu" className={styles.root}>
-      <Link
-        className="nav-link home"
-        to="home"
-        smooth={true}
-        duration={1000}
-        spy={true}
-        hashSpy={true}
-        activeClass={"active"}
-      >
-        <div className={styles.navlogo}>Home</div>
-      </Link>
-      <div className={styles.navsCont}>
+    <div
+      id="nav-menu"
+      onClick={handleMobileMenu}
+      style={{
+        color: "#7389d1",
+        backgroundColor: "#000000",
+        fontSize: "20px",
+        fontWeight: "500",
+        border: "1px solid black",
+      }}
+      className={`flex items-center justify-between px-10 py-4 border-b-2  sticky top-0 left-0 w-full	   z-[999] `}
+    >
+      <div className="flex items-center justify-between max-[900px]:w-full  mr-5 ">
+        <Link
+          className="nav-link home"
+          to="home"
+          smooth={true}
+          duration={1000}
+          spy={true}
+          hashSpy={true}
+          activeClass={"active"}
+        >
+          Home
+        </Link>
+        <span onClick={handleMobileMenu} className="hidden max-[900px]:inline">
+          <HiMenuAlt1 className="text-2xl" />
+        </span>
+      </div>
+      <div className="flex items-center gap-12 max-[900px]:hidden">
         <Link
           className="nav-link about"
           to="about"
@@ -59,6 +91,64 @@ export const Navbar = () => {
         >
           <div className={styles.nav}>contact</div>
         </Link>
+        <button className="nav-link resume" id="resume-link-1"  onClick={openNewTab}>
+          <div   className={styles.nav}>Resume</div>
+        </button>
+      </div>
+      <div className="relative min-[900px]:hidden">
+        <div
+          className={`flex gap-4 w-[170px] flex-col fixed top-[4.3rem] -right-3 p-1 px-3 shadow-[0px_13px_29px_0px_rgba(100,100,111,0.4)] bg-white h-[100vh] ${
+            isMobileMenuVisible ? "" : "-right-[300px]"
+          } transition-all duration-500`}
+        >
+          <Link
+            className="nav-link about"
+            to="about"
+            smooth={true}
+            duration={1000}
+            activeClass={styles.active}
+            spy={true}
+            hashSpy={true}
+          >
+            <div className={styles.nav}>about</div>
+          </Link>
+          <Link
+            className="nav-link skills"
+            to="skills"
+            smooth={true}
+            duration={1000}
+            activeClass={styles.active}
+            spy={true}
+            hashSpy={true}
+          >
+            <div className={styles.nav}>skills</div>
+          </Link>
+          <Link
+            className="nav-link projects"
+            to="projects"
+            smooth={true}
+            duration={1000}
+            activeClass={styles.active}
+            spy={true}
+            hashSpy={true}
+          >
+            <div className={styles.nav}>projects</div>
+          </Link>
+          <Link
+            className="nav-link contact"
+            to="contact"
+            smooth={true}
+            duration={1000}
+            activeClass={styles.active}
+            spy={true}
+            hashSpy={true}
+          >
+            <div className={styles.nav}>contact</div>
+          </Link>
+          <button className="nav-link resume" onClick={openNewTab}>
+            <div className={styles.nav}>Resume</div>
+          </button>
+        </div>
       </div>
     </div>
   );
