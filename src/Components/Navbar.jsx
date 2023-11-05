bc881f76957fdafb4b82c7a908dfe24aae52eeac
S NAVEEN KUMAR <navee1110@gmail.com>
2023-11-05 12:23:25 +0530

    nav bar id corrected

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -118,7 +118,6 @@ export const Navbar = () => {
           </div>
           <div className={styles.nav}>Resume</div>
           <a
-            id="resume-link-1"
             href="S-Naveen-Kumar-Resume.pdf"
             download="S-Naveen-Kumar-Resume.pdf"
           ></a>
